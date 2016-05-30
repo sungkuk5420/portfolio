bd8e92d247011c37e221b196debf06a8dcf5f13a
unknown <sungkuk.kim@partner.so>
2016-05-30 14:21:06 +0900

    main.js -포트폴리오 링크 연결 테스트

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -30,9 +30,10 @@ $('.menu-item').on( 'click', function() {
 	//Portfolio Modal
 	$('.open-project').on('click', function(){     
 		var projectUrl = $(this).attr("href");
-
+		projectUrl =  'https://ec2-52-34-253-229.us-west-2.compute.amazonaws.com:8000/';
 		var project = '<div class="modal fade" id="project-modal"><div class="inline-menu-container"><a id="modal-close" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></a></div><div class="modal-dialog"><div class="modal-content"></div></div></div>';
 
+		console.log(projectUrl);
 		$(project).modal({
 		  remote: projectUrl + ' #project'
 		})
