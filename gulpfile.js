a6bae4f49cb50d3817e215286d8d12d55d3d2647
Sungkuk5420 <sungkuk5420@gmail.com>
2020-02-06 11:27:11 +0900

    프리티어 자동 줄맞춤 설정.

--- gulpfile.js
+++ gulpfile.js
@@ -1,118 +1,135 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var csso = require('gulp-csso');
-var browserSync = require('browser-sync').create();
-var nodemon = require('gulp-nodemon');
-var stripDebug = require('gulp-strip-debug');
-const imagemin = require('gulp-imagemin');
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var csso = require("gulp-csso");
+var browserSync = require("browser-sync").create();
+var nodemon = require("gulp-nodemon");
+var stripDebug = require("gulp-strip-debug");
+const imagemin = require("gulp-imagemin");
 
-
-var src = 'public';
-var dist = 'public/dist';
+var src = "public";
+var dist = "public/dist";
 
 var paths = {
-    // js: src + '/javascripts/**/*.js',
-    js: src + '/javascripts/*/*.js',
-    css: src + '/stylesheets/*/*.css',
-    image: src + '/images/*/*.{jpg,png}',
-    scss: src + '/scss/*/*.scss'
+  // js: src + '/javascripts/**/*.js',
+  js: src + "/javascripts/*/*.js",
+  css: src + "/stylesheets/*/*.css",
+  image: src + "/images/*/*.{jpg,png}",
+  scss: src + "/scss/*/*.scss"
 };
 
 // Set the browser that you want to support
 const AUTOPREFIXER_BROWSERS = [
-    'ie >= 10',
-    'ie_mob >= 10',
-    'ff >= 30',
-    'chrome >= 34',
-    'safari >= 7',
-    'opera >= 23',
-    'ios >= 7',
-    'android >= 4.4',
-    'bb >= 10'
+  "ie >= 10",
+  "ie_mob >= 10",
+  "ff >= 30",
+  "chrome >= 34",
+  "safari >= 7",
+  "opera >= 23",
+  "ios >= 7",
+  "android >= 4.4",
+  "bb >= 10"
 ];
 
 // ウェブサーバーを localhost:3000 で実行する
-gulp.task('browser-sync', function (done) {
-    browserSync.init({
-        files: ['public/**/*.*', 'views/**/*.*'], // BrowserSyncにまかせるファイル群
-        proxy: 'http://localhost:3000', // express の動作するポートにプロキシ
-        port: 4000, // BrowserSync は 4000 番ポートで起動
-        open: true // ブラウザ open する
-    });
-    
-    nodemon({
-        script: 'app.js',
-        ext: 'js css',
-        ignore: [ // nodemon で監視しないディレクトリ
-            'node_modules',
-            'bin',
-            'views',
-            'public'
-        ],
-        env: {
-            'NODE_ENV': 'development'
-        },
-        stdout: false, // Express の再起動時のログを監視するため
-        done: done
-    });
+gulp.task("browser-sync", function(done) {
+  browserSync.init({
+    files: ["public/**/*.*", "views/**/*.*"], // BrowserSyncにまかせるファイル群
+    proxy: "http://localhost:3000", // express の動作するポートにプロキシ
+    port: 4000, // BrowserSync は 4000 番ポートで起動
+    open: true // ブラウザ open する
+  });
+
+  nodemon({
+    script: "app.js",
+    ext: "js css",
+    ignore: [
+      // nodemon で監視しないディレクトリ
+      "node_modules",
+      "bin",
+      "views",
+      "public"
+    ],
+    env: {
+      NODE_ENV: "development"
+    },
+    stdout: false, // Express の再起動時のログを監視するため
+    done: done
+  });
 });
 
 //nodeファイルの変更するとサーバーを自動更新
-gulp.task('server', gulp.series(gulp.parallel('browser-sync')));
+gulp.task("server", gulp.series(gulp.parallel("browser-sync")));
 
 //　Javascriptファイルを一つに併合
-gulp.task('combine-js', function () {
-    console.log(paths.js)
-    return gulp.src([
-            'public/javascripts/core/jquery-1.12.4.js',
-            'public/javascripts/core/bootstrap.min.js',
-            'public/javascripts/core/jquery.isotope.min.js',
-            'public/javascripts/core/jquery.placeholder.min.js',
-            'public/javascripts/core/contact_me.js',
-            'public/javascripts/core/smooth-scroll.js',
-            'public/javascripts/core/iscroll.js',
-            'public/javascripts/userScript.js'
-        ])
-        .pipe(stripDebug())
-        .pipe(uglify())
-        .pipe(concat('script.js'))
-        .pipe(gulp.dest(dist + '/js'));
+gulp.task("combine-js", function() {
+  console.log(paths.js);
+  return gulp
+    .src([
+      "public/javascripts/core/jquery-1.12.4.js",
+      "public/javascripts/core/bootstrap.min.js",
+      "public/javascripts/core/jquery.isotope.min.js",
+      "public/javascripts/core/jquery.placeholder.min.js",
+      "public/javascripts/core/contact_me.js",
+      "public/javascripts/core/smooth-scroll.js",
+      "public/javascripts/core/iscroll.js",
+      "public/javascripts/userScript.js"
+    ])
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(concat("script.js"))
+    .pipe(gulp.dest(dist + "/js"));
 });
 
 // sass ファイルを css にこんコンパイルする.
-gulp.task('compile-sass', function () {
-    return gulp.src([paths.scss, paths.css, 'public/stylesheets/*.css'])
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: AUTOPREFIXER_BROWSERS
-        }))
-        .pipe(csso())
-        .pipe(concat('stylesheet.css'))
-        .pipe(gulp.dest(dist + '/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+gulp.task("compile-sass", function() {
+  return gulp
+    .src([paths.scss, paths.css, "public/stylesheets/*.css"])
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: AUTOPREFIXER_BROWSERS
+      })
+    )
+    .pipe(csso())
+    .pipe(concat("stylesheet.css"))
+    .pipe(gulp.dest(dist + "/css"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
 });
 
 // image ファイルを compression する.
-gulp.task('compile-images', function () {
-    return gulp.src([paths.image, 'public/images/*.{jpg,png}'])
-        .pipe(imagemin())
-        .pipe(gulp.dest(dist + '/images'))
+gulp.task("compile-images", function() {
+  return gulp
+    .src([paths.image, "public/images/*.{jpg,png}"])
+    .pipe(imagemin())
+    .pipe(gulp.dest(dist + "/images"));
 });
 
 // ファイルの変更感知とブラウザの再起動
-gulp.task('watch', function () {
-    gulp.watch(paths.js, gulp.task('combine-js'));
-    gulp.watch(paths.scss, gulp.task('compile-sass')).on('change', browserSync.reload);
-    gulp.watch(dist + '/**');
+gulp.task("watch", function() {
+  gulp.watch(paths.js, gulp.task("combine-js"));
+  gulp
+    .watch(paths.scss, gulp.task("compile-sass"))
+    .on("change", browserSync.reload);
+  gulp.watch(dist + "/**");
 });
 
 //基本taskの設定
-gulp.task('default', gulp.series(gulp.parallel(
-    'combine-js', 'server',
-    'compile-sass', 'compile-images',
-    'watch')));
+gulp.task(
+  "default",
+  gulp.series(
+    gulp.parallel(
+      "combine-js",
+      "server",
+      "compile-sass",
+      "compile-images",
+      "watch"
+    )
+  )
+);
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 // var nodemailer = require('nodemailer');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  // $.get('/language.xls', function(data) {
-  //   console.log(data);
-  // });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.get('/colorfulTees', function(req, res, next) {
-  res.render('colorfulTees', { title: 'Express' });
+router.get("/colorfulTees", function(req, res, next) {
+  res.render("colorfulTees", { title: "Express" });
 });
 
-router.get('/xlsxText', function(req, res, next) {
-  res.render('xlsxText', { title: 'Express' });
-});
-
-router.post('/language',function(req, res, next) {
+router.post("/language", function(req, res, next) {
   console.log(res);
-  $.get('/language.xls', function(data) {
+  $.get("/language.xls", function(data) {
     console.log(data);
   });
 });
@@ -72,39 +65,38 @@ router.post('/language',function(req, res, next) {
 //   console.log(data.email);
 // });
 
-router.get('/luxuryWatch', function(req, res, next) {
-  res.render('luxuryWatch', { title: 'Express' });
-
+router.get("/luxuryWatch", function(req, res, next) {
+  res.render("luxuryWatch", { title: "Express" });
 });
 
-
 // 네이버 Papago NMT API 예제
-var client_id = 'vu8G4adE4oGZQHOrDH3C';
-var client_secret = '4_gCSpwtbz';
+var client_id = "vu8G4adE4oGZQHOrDH3C";
+var client_secret = "4_gCSpwtbz";
 // var query = "とりあえず終わる見込みがあり作業工数が発生しているものはStage設定済み（見積もり対象月の設定）。下記レポートはStageなし（見積もり予定がまだ立っていないもの）の一覧ですので共有させていただきます。";
-router.get('/translate', function (req, res) {
+router.get("/translate", function(req, res) {
   console.log(req.query.text);
-  var api_url = 'https://openapi.naver.com/v1/language/translate';
-  var request = require('request');
+  var api_url = "https://openapi.naver.com/v1/language/translate";
+  var request = require("request");
   var options = {
     url: api_url,
-    form: {'source':'ko', 'target':'ja', 'text':req.query.text},
-    headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    form: { source: "ko", target: "ja", text: req.query.text },
+    headers: {
+      "X-Naver-Client-Id": client_id,
+      "X-Naver-Client-Secret": client_secret
+    }
   };
-  request.post(options, function (error, response, body) {
-    if(response != undefined){
-
+  request.post(options, function(error, response, body) {
+    if (response != undefined) {
       if (!error && response.statusCode == 200) {
-
         // console.log(JSON.stringify(body));
         //   res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
         res.end(JSON.stringify(body));
       } else {
         res.status(response.statusCode).end();
-        console.log('error = ' + response.statusCode);
+        console.log("error = " + response.statusCode);
       }
-    }else{
-      console.log('responseresponseresponse'+error, response);
+    } else {
+      console.log("responseresponseresponse" + error, response);
     }
   });
 });
