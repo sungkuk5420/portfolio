ff2768cef9415d4920c4ed97fe3853fb9f8fa8e9
Sungkuk5420 <sungkuk5420@gmail.com>
2020-03-04 13:01:17 +0900

    번역기능 복구

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 var nodemailer = require("nodemailer");
 var dotenv = require("dotenv");
+// var translate = require("translate-google");
+
 dotenv.config();
 
 /* GET home page. */
@@ -13,12 +15,6 @@ router.get("/colorfulTees", function(req, res, next) {
   res.render("colorfulTees", { title: "colorfulTees" });
 });
 
-router.post("/language", function(req, res, next) {
-  console.log(res);
-  $.get("/language.xls", function(data) {
-    console.log(data);
-  });
-});
 router.post("/mailSend", async function(req, res, next) {
   const {
     body: { email, name, content }
@@ -53,18 +49,33 @@ router.get("/luxuryWatch", function(req, res, next) {
   res.render("luxuryWatch", { title: "luxuryWatch" });
 });
 
+// router.get("/googleTranslate", function(req, res) {
+//   const { text, sendLang, resultLang } = req.query;
+
+//   translate(text, { from: sendLang, to: resultLang })
+//     .then(translatedText => {
+//       let ja = resultLang == "ja" ? translatedText : "";
+//       let ko = resultLang == "ko" ? translatedText : "";
+//       res.json(JSON.stringify({ sendText: text, ja, ko }));
+//     })
+//     .catch(err => {
+//       console.error(err);
+//     });
+// });
 // 네이버 Papago NMT API 예제
-var client_id = "vu8G4adE4oGZQHOrDH3C";
-var client_secret = "4_gCSpwtbz";
+var client_id = "SaELp5LuZ87OYdG8LHVt";
+var client_secret = "KOq42k_ftL";
 // var query = "とりあえず終わる見込みがあり作業工数が発生しているものはStage設定済み（見積もり対象月の設定）。下記レポートはStageなし（見積もり予定がまだ立っていないもの）の一覧ですので共有させていただきます。";
 router.get("/translate", function(req, res) {
-  console.log(req.query.text);
-  var api_url = "https://openapi.naver.com/v1/language/translate";
+  const { text, sendLang, resultLang } = req.query;
+  console.log(text);
+  var api_url = "https://openapi.naver.com/v1/papago/n2mt";
   var request = require("request");
   var options = {
     url: api_url,
-    form: { source: "ko", target: "ja", text: req.query.text },
+    form: { source: sendLang, target: resultLang, text },
     headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
       "X-Naver-Client-Id": client_id,
       "X-Naver-Client-Secret": client_secret
     }
@@ -72,9 +83,11 @@ router.get("/translate", function(req, res) {
   request.post(options, function(error, response, body) {
     if (response != undefined) {
       if (!error && response.statusCode == 200) {
-        // console.log(JSON.stringify(body));
-        //   res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-        res.end(JSON.stringify(body));
+        let translatedText = JSON.parse(body).message.result.translatedText;
+        let ja = resultLang == "ja" ? translatedText : "";
+        let ko = resultLang == "ko" ? translatedText : "";
+        res.json(JSON.stringify({ sendText: text, ja, ko }));
+        res.end();
       } else {
         res.status(response.statusCode).end();
         console.log("error = " + response.statusCode);
